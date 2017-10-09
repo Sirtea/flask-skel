64340a76c474a68507fdfe698331ddae86a9798c
Gerard Monells <gerard.monells@gmail.com>
2017-10-09 16:56:20 +0200

    Env vars and listen port

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from models import db
 from admin import admin
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+MONGODB_URL = os.environ.get('MONGODB_URL')
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '123456790'
-app.config['MONGODB_SETTINGS'] = {'DB': 'shop'}
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['MONGODB_SETTINGS'] = {'host': MONGODB_URL}
 db.init_app(app)
 admin.init_app(app)
--- server.sh
+++ server.sh
@@ -3,4 +3,6 @@
 PYTHONDONTWRITEBYTECODE=" " \
 FLASK_DEBUG=1 \
 FLASK_APP=app.py \
-flask run
+MONGODB_URL='mongodb://localhost:27017/test' \
+SECRET_KEY='1234567890' \
+flask run --host 0.0.0.0 --port 8080
