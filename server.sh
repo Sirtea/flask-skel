a4f2836833db77c3004075ffac1356f7e277d7ae
Gerard Monells <gerard.monells@gmail.com>
2019-09-19 11:16:38 +0200

    Split app folder

--- server.sh
+++ server.sh
@@ -2,7 +2,7 @@
 
 PYTHONDONTWRITEBYTECODE=" " \
 FLASK_DEBUG=1 \
-FLASK_APP=app.py \
+FLASK_APP=app/app:app \
 MONGODB_URL='mongodb://localhost:27017/test' \
 SECRET_KEY='1234567890' \
 flask run --port 8080
